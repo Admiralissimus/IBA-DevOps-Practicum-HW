bb0061acb6a0b72c4c55c3edf1ea61717b2fa400
Aliaksandr Ushakou <aushakou@gmail.com>
2023-07-09 19:41:33 +0300

    Added slave node1

--- task1.sh
+++ task1.sh
@@ -11,3 +11,6 @@ curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee  
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]   https://pkg.jenkins.io/debian-stable binary/ | sudo tee   /etc/apt/sources.list.d/jenkins.list > /dev/null
 apt-get update
 apt-get -y install jenkins
+
+echo 'Export key to var $JENKINS_SERVER_SSH_KEY_PUB in nodes'
+
--- task2-node.sh
+++ task2-node.sh
@@ -6,4 +6,10 @@ echo "Installing java"
 sudo apt -y install openjdk-17-jre
 java -version
 
+echo "Creating unprev user 'jenkins'"
+sudo useradd -m -s /bin/bash jenkins
+
+su jenkins
+mkdir -p ~/.ssh
+echo "$JENKINS_SERVER_SSH_KEY_PUB" >> ~/.ssh/authorized_keys
 
